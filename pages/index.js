ecdddcbd5699a910c485d1789f15bc1897fe7e00
unknown <quang115thd@gmail.com>
2023-10-03 16:48:56 +0700

    update first

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next Edit</title>
+        <title>Create Next New</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
