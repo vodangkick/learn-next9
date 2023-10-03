0bcc1e052345bb0f5dd34f4d93f28a4654cc2b00
unknown <quang115thd@gmail.com>
2023-10-03 16:55:10 +0700

    push develop

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next New</title>
+        <title>Create Next Develop</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
