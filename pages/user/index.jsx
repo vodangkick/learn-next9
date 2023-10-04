9c03eacd1eb7b13217086e84294605b4724d51a4
unknown <quang115thd@gmail.com>
2023-10-04 16:48:54 +0700

    fix image

--- pages/user/index.jsx
+++ pages/user/index.jsx
@@ -1,15 +1,19 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
+import Image from 'next/image'
+import ImageNew from '../../assets/images/corgi.jpg';
+
 
 export default function index() {
   const route = useRouter();
-  
+
   return (
     <div>
-        <Link href="/post/121">
-            <span>Post</span>
-        </Link>
+      <Image src={ImageNew} width={300} alt="asdsd" priority />
+      <Link href="/post/121">
+        <span>Post</span>
+      </Link>
     </div>
-  )
+  );
 }
